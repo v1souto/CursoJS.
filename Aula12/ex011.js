43cb4c40f89bc7b7d910f6afb24ba1e32e74bd7d
v1souto <souto2096@gmail.com>
2026-03-05 10:14:11 -0300

    s

--- Aula12/ex011.js
+++ Aula12/ex011.js
@@ -1,6 +1,9 @@
 var idade = 22
-if (idade < 18) {
-    console.log('Menor de idade') 
+console.log(`Voce tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('nao vota') 
+} else if (idade <18 || idade >65) {
+    console.log('voto opcional')
 } else {
-    console.log('Maior de idade')
+    console.log(`voto obrigatorio`)
 }
---
+++ Aula12/ex012.js
@@ -0,0 +1,10 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora sao exatamente ${hora}horas.`)
+if (gora < 12) {
+    console.log('Bom dia')
+} else if (hora <= 18) {
+    console.log ('Boa tarde')
+} else {
+    console.log('boa noite')
+}
